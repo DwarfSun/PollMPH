88469d2df6184c5f0029b9a56f668175fdcae97a
Matt Whitwam <matt@dwarfsun.co.za>
2018-07-15 16:53:39 +0200

    Let's get this party started shall we

---
+++ excavator/CoinStats.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPH_Poll
+{
+  public class CoinStats
+  {
+    public string coinName;
+    public string host;
+    public string hostList;
+    public long port;
+    public string directMiningHost;
+    public string directMiningHostList;
+    public long directMiningPort;
+    public string algorithm;
+    public double normalizedProfit;
+    public double normalizedProfitAmd;
+    public double normalizedProfitNvidia;
+    public double profit;
+    public string poolHash;
+    public string netHash;
+    public double difficulty;
+    public double reward;
+    public long lastBlock;
+    public long timeSinceLastBlock;
+    public string timeSinceLastBlockInWords;
+    public double bittrexBuyPrice;
+    public double cryptsyBuyPrice;
+    public double yobitBuyPrice;
+    public double poloniexBuyPrice;
+    public double highestBuyPrice;
+
+    public CoinStats()
+    {
+    }
+  }
+}
---
+++ excavator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MPH_Poll
+{
+  class Program
+  {
+    private static List<CoinStats> coins = new List<CoinStats>();
+    private static void FetchData()
+    {
+      coins = QueryMph.GetCoinStats().Result;
+    }
+
+    public static void Main(string[] args)
+    {
+      FetchData();
+      coins = coins.OrderByDescending(o => o.normalizedProfitNvidia).ToList();
+
+      foreach (CoinStats coin in coins)
+      {
+        Console.WriteLine("{0} {1} {2}", coin.coinName, coin.host, coin.port);
+      }
+      Console.WriteLine();
+    }
+  }
+}
