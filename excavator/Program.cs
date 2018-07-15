62f6d16cab9a5f89748cc011d89d6798960698ec
Matt Whitwam <matt@dwarfsun.co.za>
2018-07-15 17:05:28 +0200

    Let's do that again

--- excavator/Program.cs
+++ excavator/Program.cs
@@ -1,32 +1,145 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
+using MPH_Poll;
 
-namespace MPH_Poll
+namespace mph_switcher
 {
+  public class Miner
+  {
+    public string algorithm;
+    public string filename;
+    public string arguments;
+  }
+  public class Config
+  {
+    public string user;
+    public string rig;
+    public List<string> exclusions = new List<string>();
+    public List<Miner> miners = new List<Miner>();
+
+    public Config()
+    {
+      try
+      {
+        string jsonFile = File.ReadAllText("./config.json");
+        dynamic config = JsonConvert.DeserializeObject(jsonFile);
+        user = config.user;
+        rig = config.rig;
+        foreach (string algo in config.exclude)
+        {
+          exclusions.Add(algo);
+        }
+        foreach (dynamic m in config.miners)
+        {
+          Miner miner = new Miner
+          {
+            algorithm = m.algorithm,
+            filename = m.filename,
+            arguments = m.arguments
+          };
+          miners.Add(miner);
+        }
+      }
+      catch
+      {
+        System.Console.Error.WriteLine("ERROR: Unable to read config.json");
+      }
+    }
+  }
+
   class Program
   {
+    private static Config config = new Config();
+    private static CoinStats topCoin = new CoinStats();
     private static List<CoinStats> coins = new List<CoinStats>();
     private static void FetchData()
     {
       coins = QueryMph.GetCoinStats().Result;
+      foreach (string ex in config.exclusions)
+      {
+        coins.RemoveAll(o => o.algorithm == ex);
+      }
+      coins = coins.OrderByDescending(o => o.normalizedProfitNvidia).ToList();
     }
 
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
-      FetchData();
-      coins = coins.OrderByDescending(o => o.normalizedProfitNvidia).ToList();
+      Process process = null;
+      int loops = 0;
 
-      foreach (CoinStats coin in coins)
+      while (true)
       {
-        Console.WriteLine("{0} {1} {2}", coin.coinName, coin.host, coin.port);
+        loops++;
+        bool fetched = false;
+        while (fetched==false)
+        {
+          try
+          {
+            FetchData();
+            fetched = true;
+          }
+          catch 
+          { 
+            System.Console.Error.WriteLine("Polling multipoolhub failed, trying again...");
+            System.Threading.Thread.Sleep(5000);
+          }
+        }
+
+        if (topCoin.coinName == coins[0].coinName)
+        {
+          if (loops % 5 == 0)
+            System.Console.WriteLine("{0} is still the most profitable coin.", topCoin.coinName);
+        }
+        else
+        {
+          try
+          {
+            if (process != null)
+            {
+              System.Console.WriteLine("{0} is not the most profitable coin, with a normalised profit rating of {1}", topCoin.coinName, topCoin.normalizedProfitNvidia);
+              process.Kill();
+            }
+          }
+          catch
+          {
+            System.Console.Error.WriteLine("Failed to terminate process");
+          }
+          try
+          {
+            CoinStats coin = coins[0];
+            Miner miner = config.miners.Find(x => x.algorithm == coin.algorithm);
+            miner.arguments = string.Format(miner.arguments, coin.host, coin.port, config.user, config.rig);
+
+            process = new Process
+            {
+              StartInfo = new ProcessStartInfo
+              {
+                FileName = miner.filename,
+                Arguments = miner.arguments,
+                RedirectStandardOutput = false,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+              }
+            };
+            process.Start();
+          }
+          catch
+          {
+            System.Console.Error.WriteLine("Failed to start process");
+          }
+        }
+        try
+        {
+          topCoin = coins[0];
+          System.Console.WriteLine("Mining {0}, with a normalised profit rating of {1}", topCoin.coinName, topCoin.normalizedProfitNvidia);
+        }
+        catch { }
+        System.Threading.Thread.Sleep(60000);
       }
-      Console.WriteLine();
     }
   }
 }
--- excavator/obj/Debug/netcoreapp2.1/excavator.AssemblyInfo.cs
+++ excavator/obj/Debug/netcoreapp2.1/excavator.AssemblyInfo.cs
@@ -9,6 +9,8 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("excavator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Helper program for managing several 3rd party crypto miner apps mining multiple c" +
+"urrencies")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("excavator")]
