74c8f866153442b1dc8ebbdd452d3143ef95ca5f
Matt Whitwam <matt@dwarfsun.co.za>
2018-07-15 17:45:17 +0200

    Fixed moronic typo

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ then
 fi
 
 #Install the .NET Core SDK
-wget -q https://packages.microsoft.com/config/ubuntu/$(lbv_release -r -s)/packages-microsoft-prod.deb
+wget -q https://packages.microsoft.com/config/ubuntu/$(lsb_release -r -s)/packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
 apt-get --assume-yes install apt-transport-https
 apt-get update
